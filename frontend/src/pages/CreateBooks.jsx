996b5f8bb090635761fd3c5032c220d57aba174b
aleksandrre <108459639+aleksandrre@users.noreply.github.com>
2023-11-30 01:00:16 +0400

    Update CreateBooks.jsx

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -19,7 +19,7 @@ const CreateBooks = () => {
     };
     setLoading(true);
     axios
-      .post("http://localhost:5555/books", data)
+      .post("https://merapp.onrender.com/books", data)
       .then(() => {
         setLoading(false);
         navigate("/");
